96e00ead2b483e18fa59bfa8ec5da59dcf808329
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2024-02-21 19:13:08 +0100

    add access from specific ip address (not work now)

--- photo-app-api-users/src/main/java/com/adjoda/photoapp/api/users/security/WebSecurity.java
+++ photo-app-api-users/src/main/java/com/adjoda/photoapp/api/users/security/WebSecurity.java
@@ -1,7 +1,9 @@
 package com.adjoda.photoapp.api.users.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,18 +11,26 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+
+@RequiredArgsConstructor
 public class WebSecurity {
+
+    private final Environment environment;
+
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
 
-        http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+        http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, "/api/v1/users")
+                        .permitAll()
+                        //.access(new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip")+"')"))
                         .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll())
 
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
