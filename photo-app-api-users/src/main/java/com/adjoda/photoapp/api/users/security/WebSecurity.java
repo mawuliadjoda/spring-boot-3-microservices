dcff1add9370a8d975fa0982fda1529ef3f43d74
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2024-02-21 18:27:24 +0100

    add spring security and configuration but allow POST endpoint

---
+++ photo-app-api-users/src/main/java/com/adjoda/photoapp/api/users/security/WebSecurity.java
@@ -0,0 +1,33 @@
+package com.adjoda.photoapp.api.users.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console")).permitAll())
+
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        http.headers(header->{header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);});
+
+        return http.build();
+    }
+}
