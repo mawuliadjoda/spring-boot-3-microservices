74bea4cd12a0d57e4b2d4988b77a6a08fc578918
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2024-03-06 07:22:29 +0100

    reoganize project

---
+++ project1/users-ws/src/main/java/com/adjoda/api/users/UsersWsApplication.java
@@ -0,0 +1,13 @@
+package com.adjoda.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersWsApplication.class, args);
+	}
+
+}
